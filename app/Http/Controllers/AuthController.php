4612d46d100bf3eb1c4d3999014ae7b31a8372a7
enriquesousa <enrique.sousa@gmail.com>
2022-01-20 12:04:15 -0800

    commit inicial

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    public function signup(Request $request){
+        $validateData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        ]);
+    
+        $data = array();
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        DB::table('users')->insert($data);
+        return $this->login($request);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+}
---
+++ resources/js/components/auth/login.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card shadow-sm my-5">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="login-form">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                </div>
+
+                                    <form class="user">
+
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Correo Electrónico">
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <input type="password" class="form-control" id="exampleInputPassword" placeholder="Contraseña">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small"
+                                                style="line-height: 1.5rem;">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Recordarme</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <a href="index.html" class="btn btn-primary btn-block">Login</a>
+                                        </div>
+                                        
+                                        <hr>
+                                        
+                                    </form>
+
+
+                                    <div class="text-center">
+                                        <router-link to="/register" class="font-weight-bold small">Crear Cuenta!</router-link>
+                                    </div>
+                                    <div class="text-center">
+                                        <router-link to="/forget" class="font-weight-bold small">Olvidaste Contraseña?</router-link>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<style>
+
+</style>
